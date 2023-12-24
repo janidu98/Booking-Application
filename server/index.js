af717954b4c7a39d370dd9f85bea3e1ad05fec27
janidu98 <janinduchathushan@gmail.com>
2023-12-24 23:47:14 +0530

    create hotel model and create controllers file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import usersRoute from './routes/users.js';
+import roomsRoute from './routes/rooms.js';
+import hotelsRoute from './routes/hotels.js';
+import authRoute from './routes/auth.js';
 
 dotenv.config();
 
@@ -11,6 +15,11 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 })
 
 const app = express();
+
+app.use('/api/auth', authRoute)
+app.use('/api/user', usersRoute)
+app.use('/api/rooms', roomsRoute)
+app.use('/api/hotels', hotelsRoute)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
