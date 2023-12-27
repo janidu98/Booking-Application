bb3c32e89c36c735215a96891f0ad46182b7314f
janidu98 <janinduchathushan@gmail.com>
2023-12-27 23:56:21 +0530

    create error handler middleware

--- server/index.js
+++ server/index.js
@@ -23,6 +23,18 @@ app.use('/api/user', usersRoute)
 app.use('/api/rooms', roomsRoute)
 app.use('/api/hotels', hotelsRoute)
 
+//error middleware
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || 'Internal server error';
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    })
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
