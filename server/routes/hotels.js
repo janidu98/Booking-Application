b0e50fe13b20332a4596e779fec4f7934b2e66c8
janidu98 <janinduchathushan@gmail.com>
2024-01-06 13:55:32 +0530

    add two routes for hotel route

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -1,18 +1,20 @@
 import express from 'express';
-import { createHotel, deleteHotel, getAllHotels, getHotel, updateHotel } from '../controllers/hotelControllers.js';
+import { countByCity, countByType, createHotel, deleteHotel, getAllHotels, getHotel, updateHotel } from '../controllers/hotelControllers.js';
 import { verifyAdmin } from '../utils/verifyToken.js';
 
 const route = express.Router();
 
 //create hotel
-route.post('/', verifyAdmin, createHotel);
+route.post('/create', verifyAdmin, createHotel);
 //update hotel
-route.put('/:id', verifyAdmin, updateHotel);
+route.put('/update/:id', verifyAdmin, updateHotel);
 //delete hotel
-route.delete('/:id', verifyAdmin, deleteHotel);
+route.delete('/delete/:id', verifyAdmin, deleteHotel);
 //get hotel
-route.get('/:id', getHotel);
+route.get('/get/:id', getHotel);
 //get all hotels
-route.get('/', getAllHotels);
+route.get('/getAll', getAllHotels);
+route.get('/countByCity', countByCity);
+route.get('/countByType', countByType);
 
 export default route;
